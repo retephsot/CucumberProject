75b82248441c1a9763d384a44d08f12650838d47
retephsot <synhlee@yahoo.com>
2016-10-14 15:08:06 -0400

    adding both html and json reporting

--- src/test/java/com/cucumber/CucumberProject/runnerTest.java
+++ src/test/java/com/cucumber/CucumberProject/runnerTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		// use "html:target/html/" or "json:target/json/output.json"
-		format = {"pretty", "json:target/json/output.json"},
+		format = {"pretty", "json:target/json/output.json", "html:target/html/"},
 		features = "src/test/resource"
 		)
 public class runnerTest {
